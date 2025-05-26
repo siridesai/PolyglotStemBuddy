8b8aeb359cf9e267a1d2593878adf1f9edfd7ede
siridesai <76456899+siridesai@users.noreply.github.com>
2025-05-26 00:30:15 -0700

    Update Vite configuration for @react-pdf/renderer

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@react-pdf/renderer']
+    include: ['@react-pdf/renderer'],
+    exclude: ['@react-pdf/font', '@react-pdf/layout']
   },
   build: {
     commonjsOptions: {
-      include: [/node_modules/]
+      include: [/node_modules/],
+      transformMixedEsModules: true
     }
   }
 })
