d6bcd7fcfee3cd0f0b401b3e1b2ede0334dc2dc9
siridesai <76456899+siridesai@users.noreply.github.com>
2025-05-26 00:30:49 -0700

    Fix unicode-trie module export error

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@react-pdf/renderer', 'base64-js'],
+    include: ['@react-pdf/renderer', 'base64-js', 'unicode-trie'],
     exclude: ['@react-pdf/font', '@react-pdf/layout']
   },
   build: {
