9ddc1eecac5b23474ff4d7616d76a028f87fde66
siridesai <76456899+siridesai@users.noreply.github.com>
2025-05-26 00:30:31 -0700

    Fix base64-js module export issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@react-pdf/renderer'],
+    include: ['@react-pdf/renderer', 'base64-js'],
     exclude: ['@react-pdf/font', '@react-pdf/layout']
   },
   build: {
     commonjsOptions: {
-      include: [/node_modules/],
+      include: [/node_modules/, /node_modules\/.*base64-js.*/],
       transformMixedEsModules: true
     }
   }
