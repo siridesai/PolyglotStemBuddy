ed04d4ed2668e7060e22be8d1e0f603d8cee62c7
Siri <siridesai@outlook.com>
2025-06-28 12:02:41 -0700

    Reworked code to fix for bugs.

--- src/services/server.js
+++ src/services/server.js
@@ -28,11 +28,6 @@ const MAX_MESSAGE_LENGTH = 1500;  // Truncate long messages
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 
 const sessionRunMap = new Map();
 
