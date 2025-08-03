acadfc854881a1efc0d942e9e0ffd4556291a416
siridesai <76456899+siridesai@users.noreply.github.com>
2025-08-03 14:03:10 -0700

    Update About.tsx

--- src/About.tsx
+++ src/About.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import Head from 'next/head';
 
 const GITHUB_RAW_MD =
 "https://raw.githubusercontent.com/siridesai/PolyglotStemBuddy/refs/heads/main/about.md" ;
@@ -14,22 +13,13 @@ const About: React.FC = () => {
       .then((res) => res.text())
       .then(setContent);
   }, []);
-
-   const metaDescription =
-    "Polyglot STEM Buddy is an open-source educational web application designed to make STEM learning accessible to everyone, in multiple languages. Created by Siri Mutalik Desai";
-
+  
   return (
-    <>
-      <Head>
-        <title>About Polyglot STEM Buddy – Siri Mutalik Desai</title>
-        <meta name="description" content={metaDescription} />
-      </Head>
     <div className="min-h-screen bg-sketch-doodles flex flex-col">
       <div className="prose text-black max-w-3xl mx-auto px-4 py-10 flex-1">
         <ReactMarkdown>{content}</ReactMarkdown>
       </div>
     </div>
-    </>
   );
       
 };
